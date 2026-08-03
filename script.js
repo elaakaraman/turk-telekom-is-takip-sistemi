e94458d67ddfc4702d976b6fd497c12b2545e3eb
elaakaraman <elanr.krmn00@gmail.com>
2026-08-03 10:11:24 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 function binaAra() {
     const binaId = document.getElementById("binaId").value;
-
     const sonucAlani = document.getElementById("sonucAlani");
 
     if (binaId === "") {
-        sonucAlani.innerHTML = "<p>Lütfen bir Bina ID giriniz.</p>";
+        sonucAlani.innerHTML = `
+            <p>Lütfen bir Bina ID giriniz.</p>
+        `;
+    } else if (binaId === "101") {
+        sonucAlani.innerHTML = `
+            <h3>Bina ID: 101</h3>
+
+            <div class="proje-karti">
+                <h3>Fiber Altyapı Projesi</h3>
+                <p><strong>Santral Adı:</strong> Keçiören Santrali</p>
+                <p><strong>Proje Yılı:</strong> 2026</p>
+                <p><strong>Proje Durumu:</strong> Devam Ediyor</p>
+            </div>
+        `;
     } else {
-        sonucAlani.innerHTML =
-            "<h3>Bina ID: " + binaId + "</h3>" +
-            "<p>Bu binaya ait proje bilgileri daha sonra burada gösterilecektir.</p>";
+        sonucAlani.innerHTML = `
+            <h3>Bina ID: ${binaId}</h3>
+            <p>Bu Bina ID'ye ait proje bulunamadı.</p>
+        `;
     }
 }
