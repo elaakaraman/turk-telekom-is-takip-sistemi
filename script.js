aaa3c0014336c85f2b055d8fdf86435b7f709a02
elaakaraman <elanr.krmn00@gmail.com>
2026-08-03 10:06:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+function binaAra() {
+    const binaId = document.getElementById("binaId").value;
 
+    const sonucAlani = document.getElementById("sonucAlani");
+
+    if (binaId === "") {
+        sonucAlani.innerHTML = "<p>Lütfen bir Bina ID giriniz.</p>";
+    } else {
+        sonucAlani.innerHTML =
+            "<h3>Bina ID: " + binaId + "</h3>" +
+            "<p>Bu binaya ait proje bilgileri daha sonra burada gösterilecektir.</p>";
+    }
+}
